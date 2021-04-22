d2a9fe1042e3fb50af3317cda3dac44e05b74c50
Athul Cyriac Ajay <athul8720@gmail.com>
2021-04-22 21:14:48 +0530

    Initialize

---
+++ accounting/accounting/doctype/general_ledger/general_ledger.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Athul Cyriac Ajay and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+from frappe.utils import today()
+from frappe.model.document import Document
+
+class GeneralLedger(Document):
+	def validate(self):
+		self.posting_date = today()
---
+++ accounting/accounting/doctype/general_ledger/test_general_ledger.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Athul Cyriac Ajay and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestGeneralLedger(unittest.TestCase):
+	pass
